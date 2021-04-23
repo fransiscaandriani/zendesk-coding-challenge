f8c89a1fdb006358295f6e23082f8edf1581deee
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2021-04-23 19:22:55 +0800

    Read txt file
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,8 +1,23 @@
 package com;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        try {
+            final File myObj = new File("input/testCase1.txt");
+            final Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                final String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.printf("File specified does not exist.");
+        }
     }
 }
