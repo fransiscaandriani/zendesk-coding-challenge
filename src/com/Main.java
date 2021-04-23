67782b1ea505975cf42e58a9686a1d1a52cc99cb
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2021-04-23 19:36:56 +0800

    Error checking for empty file and for first line
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,22 +2,48 @@ package com;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         try {
-            final File myObj = new File("input/testCase1.txt");
-            final Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                final String data = myReader.nextLine();
+            // Read txt file
+            final File fileInput = new File("input/testCase1.txt");
+            final Scanner scanner = new Scanner(fileInput);
+
+            // Check that file is not empty
+            if(!scanner.hasNextLine()) {
+                System.out.println("Error: File input is empty");
+                return;
+            }
+
+            // Read first line
+            final String firstLine = scanner.nextLine();
+
+            // Get car and motorcycle lot sizes
+            final List<Integer> lotSizes = Arrays.stream(firstLine.split(" "))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            // Check if lot sizes are valid
+            if(lotSizes.size() != 2 || lotSizes.get(0) == null || lotSizes.get(1) == null
+            || lotSizes.get(0) < 0 || lotSizes.get(1) < 0) {
+                System.out.println("Error: Invalid first line. First line should contain two integers indicating the " +
+                        "size of car parking lot and the size of motorcycle parking lot respectively");
+                return;
+            }
+            
+            while (scanner.hasNextLine()) {
+                final String data = scanner.nextLine();
                 System.out.println(data);
             }
-            myReader.close();
+            scanner.close();
         } catch (FileNotFoundException e) {
-            System.out.printf("File specified does not exist.");
+            System.out.println("Error: File specified does not exist.");
         }
     }
 }
