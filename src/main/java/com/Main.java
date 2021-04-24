7fd2f93cf0e70d52acde54c5c9dac7bfe59e8515
Fransisca Andriani <fran0025@e.ntu.edu.sg>
2021-04-24 17:49:46 +0800

    Parse vehicle data for each line after the first line
    Signed-off-by: Fransisca Andriani <fran0025@e.ntu.edu.sg>

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -28,12 +29,14 @@ public class Main {
             // Read first line to get parking lot sizes
             final String firstLine = scanner.nextLine();
             final List<Integer> lotSizes = FileParsingUtil.parseParkingLotSizes(firstLine);
-            
+
             // Make new parking lot
             final ParkingLot parkingLot = new ParkingLot(lotSizes.get(0), lotSizes.get(1));
 
+            // Parse each line after the first line
             while (scanner.hasNextLine()) {
                 final String data = scanner.nextLine();
+                final Map<String, String> vehicleData = FileParsingUtil.parseVehicleInputLine(data);
                 System.out.println(data);
             }
             scanner.close();
--- src/main/java/com/util/FileParsingUtil.java
+++ src/main/java/com/util/FileParsingUtil.java
@@ -1,13 +1,17 @@
 package com.util;
 
 import com.exception.FileParsingException;
+import com.model.VehicleEntry;
+import com.model.VehicleType;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FileParsingUtil {
-    public static List<Integer> parseParkingLotSizes(String line) throws FileParsingException{
+    public static List<Integer> parseParkingLotSizes(final String line) throws FileParsingException{
         // Get car and motorcycle lot sizes
         final List<Integer> lotSizes = Arrays.stream(line.split(" "))
                 .map(Integer::parseInt)
@@ -21,5 +25,85 @@ public class FileParsingUtil {
         } else {
             return lotSizes;
         }
+    }
+
+    public static Map<String, String> parseVehicleInputLine(final String line) throws FileParsingException{
+        // Get each individual data from the string
+        final List<String> data = Arrays.asList(line.split(" "));
+
+        final Map<String, String> result = new HashMap<>();
+
+        // If this is an Enter line
+        if(data.get(0).equals("Enter") && data.size() == 4){
+            result.putAll(getEnteringVehicleData(data));
+        }
+        // if this is an Exit line
+        else if (data.get(0).equals("Exit") && data.size() == 3) {
+            result.putAll(getExitingVehicleData(data));
+        }
+        // Otherwise, the line is invalid
+        else {
+            throw new FileParsingException("Error: Vehicle input line is in invalid format.");
+        }
+        return result;
+    }
+
+    private static Map<String, String> getEnteringVehicleData (final List<String> data) throws FileParsingException {
+        final Map<String, String> result = new HashMap<>();
+        result.put("Direction", data.get(0));
+
+        // Check if Vehicle Type is valid
+        if(checkIfVehicleTypeIsValid(data.get(1))) {
+            result.put("VehicleType", data.get(1));
+        } else {
+            throw new FileParsingException("Error: Vehicle type specified does not exist.");
+        }
+
+        // Check if Vehicle Number is valid
+        if(checkIfVehicleNumberIsValid(data.get(2))) {
+            result.put("VehicleNumber", data.get(2));
+        } else {
+            throw new FileParsingException("Error: Vehicle number is invalid.");
+        }
+
+        // Check if timestamp is valid
+        if(checkIfTimestampIsValid(data.get(3))) {
+            result.put("Timestamp", data.get(3));
+        } else {
+            throw new FileParsingException("Error: Timestamp is invalid.");
+        }
+        return result;
+    }
+
+    private static Map<String, String> getExitingVehicleData(final List<String> data) throws FileParsingException {
+        final Map<String, String> result = new HashMap<>();
+        result.put("Direction", data.get(0));
+
+        // Check if Vehicle Number is valid
+        if(checkIfVehicleNumberIsValid(data.get(1))) {
+            result.put("VehicleNumber", data.get(1));
+        } else {
+            throw new FileParsingException("Error: Vehicle number is invalid.");
+        }
+
+        // Check if timestamp is valid
+        if(checkIfTimestampIsValid(data.get(2))) {
+            result.put("Timestamp", data.get(2));
+        } else {
+            throw new FileParsingException("Error: Timestamp is invalid.");
+        }
+        return result;
+    }
+
+    private static boolean checkIfVehicleTypeIsValid(final String vehicleTypeString) {
+        return Arrays.asList(VehicleType.values()).contains(vehicleTypeString);
+    }
+
+    private static boolean checkIfVehicleNumberIsValid(final String vehicleNumberString) {
+        return vehicleNumberString.matches("/[A-Za-z]{3}[\\d]{4}[A-Za-z]/");
+    }
+
+    private static boolean checkIfTimestampIsValid(final String timestampString) {
+        return timestampString.matches("^\\d*$");
     }
 }
